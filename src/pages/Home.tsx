649a70646ac1711d3b13cfac3a510364932ddd5f
John Doe <hamedhasan422421@gmail.com>
2023-07-14 13:25:26 +0300

    Import the necessary icons and components from the respective libraries. Add a description of the team's strength using appropriate headings and text formatting. Display an image representing strength using the provided image file. Implement a slide-in animation effect for the image and the content. Create a list of key features using checkboxes and corresponding descriptions. Apply appropriate styling to the component and its elements.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import Banner from '../components/Banner/Banner';
-
+import Faciliteis from '../components/Facilities/Faciliteis';
+import CorporateClient from '../components/CorporateClient/CorporateClient'
+import Strength from '../components/Strength/Strength';
 const Home = () => {
     return (
         <div>
             <Banner/>
+            <Faciliteis/>
+            <CorporateClient/>
+            {/* product compo */}
+            <Strength/>
         </div>
     );
 };
